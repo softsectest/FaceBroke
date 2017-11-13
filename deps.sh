aa67e3c75bda7331a23c5d31c50477ec8a8ff86a
Matt Yaraskavitch <myaraskavitch@softwaresecured.com>
2017-11-13 12:23:40 -0500

    Updated dependencies

--- deps.sh
+++ deps.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-sudo yum install -y htop docker git htop
+sudo yum install -y htop docker git tmux
 
 sudo curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 
